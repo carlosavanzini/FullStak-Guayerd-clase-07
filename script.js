81599ce14937a863139be9f4a0c0db7ee2f2e43a
avanzinidesarrollo@gmail.com <avanzinidesarrollo@gmail.com>
2021-06-15 12:12:32 -0300

    commit

--- script.js
+++ script.js
@@ -19,37 +19,40 @@ let vuelto;
 let caja = 2000;
 let retiro;
 let total;
-let tarjeta;
-    while(menu!=5){
+let tarjeta=0;
+let monto;
+let balance;
+    while(menu!=5){ //pide los precios de los prodcutos que vamos a llevar
         menu = parseInt(prompt("¿Que operacion desea realizar?\n1.Cobrar\n2.Retirar dinero de la caja\n3.Ver balance de caja\n4.Ver los montos totales de todas las operaciones realizadas\n5.Salir"));
   switch (menu) {
-    case 1:
+    case 1: // Cobrar
       do {
-           precio = parseInt(prompt("Ingrese el precio del producto, para finalizar ingrese 0"));
-           suma = suma + precio;
-           alert(`El total es igual a ${suma} pesos`);
+              precio = parseInt(prompt("Ingrese el precio del producto, para finalizar ingrese 0"));
+              suma = suma + precio;
+              alert(`El total es igual a ${suma} pesos`);
       } while (precio != 0);
+
           let abono = prompt("¿Con qué desea abonar? tarjeta o efectivo");
-          if (abono === "efectivo" && suma > 2000) {
-          descuento = suma * 0.05;
-          suma = suma - descuento;
-          alert(suma);
-      } else if (abono === "efectivo" && suma > 5000) {
-          descuento = suma * 0.10;
-          suma = suma - descuento;
-          alert(`El monto con descuento incluido es ${suma}`);
+           if (abono === "efectivo" && suma > 2000) {
+              descuento = suma * 0.05;
+              suma = suma - descuento;
+              alert(`El monto a pagar con descuento es de ${suma}`);}
+            else{
+              alert(`El monto a pagar sin descuento es de ${suma}`);
+            }
+          if (abono === "efectivo" && suma > 5000) {
+              descuento = suma * 0.10;
+              suma = suma - descuento;
+              alert(`El monto con descuento incluido es ${suma}`);
       }
-        else if ( abono === "tarjeta" && suma > 5000){
-          descuento= suma *0.10;
-          tarjeta= suma-descuento;
-          alert(`El monto a pagar con descuento es ${tarjeta}`)
-          
-        }else{
-          tarjeta=suma
-          alert(`El monto a pagar sin descuento  es de ${tarjeta}`)
+          else if ( abono === "tarjeta" && suma > 5000){
+              descuento= suma *0.10;
+              tarjeta= suma-descuento;
+              alert(`El monto a pagar con descuento es ${tarjeta}`)
+              
         }
-      if (abono === "efectivo") {
-          pago = parseInt(prompt('¿Con cuánto quiere pagar?'));               
+           if (abono === "efectivo") {
+            pago = parseInt(prompt('¿Con cuánto quiere pagar?'));               
             if (pago >= suma) {
               vuelto = pago - suma;
               alert('Su vuelto es: ' + vuelto);              
@@ -58,17 +61,36 @@ let tarjeta;
         }
      
       }
-      break;    
-    case 2:
+         break;    
+    case 2://retirar dinero de la caja
         retiro = parseInt(prompt('¿Cuánto dinero desea retirar?'));
-        //total = caja + suma;
-        if (caja > retiro) {
-                total = caja - retiro;
-                alert(total);
+        totalEfectivoCaja = caja + suma;
+        if (totalEfectivoCaja > retiro) {
+                retirocaja = totalEfectivoCaja - retiro;
+                alert(`Usted retiro${retiro} pesos y hora su saldo de caja es ${totalEfectivoCaja}`);
         }else{
-                alert('Fondos insuficientes');
+                alert(`Fondos insuficientes, usted quiso retirar ${retiro} y su fondo de caja es de ${totalEfectivoCaja}`);
         }
-      break;    
+      break; 
+      
+    case 3:// balance de caja
+        balance = (parseInt(prompt("Ver con que se pago:\n1.Efectivo\n2.Tarjeta")));
+        if (balance===1) {
+          alert(`Se pago con efectivo una suma de ${suma} pesos`);
+        }else if(balance===2 && tarjeta!=0) {
+          alert(`se pago con tarjeta un monto de ${tarjeta}`)
+        } else{
+          alert("Por el momento no hay compras con tarjetas")
+        }
+      break;
+    case 4:// monto total de las operaciones
+      monto = (parseInt(prompt("¿Que total de monto de operaciones quiere ver?\n1.Efectivo\n2.Tarjeta")));
+      if (balance===1) {
+        alert(`Se pago con efectivo una suma de ${suma} pesos`);
+      } else {
+        alert(`se pago con tarjeta un monto de ${tarjeta}`)
+      }
+      break;
   
     default:
       alert("Gracias por usar Nuestro sistema");
