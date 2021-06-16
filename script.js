1cc9012af7bc9f8830797608175504282a7ccee3
avanzinidesarrollo@gmail.com <avanzinidesarrollo@gmail.com>
2021-06-16 10:13:53 -0300

    commit

--- script.js
+++ script.js
@@ -16,7 +16,6 @@ let precio;
 let menu;
 let pago;
 let vuelto;
-let retiro;
 let tarjeta=0;
 let monto;
 let balance;
@@ -26,95 +25,97 @@ let totalVentasEfectivo=0;
 let totalVentasTarjeta=0;
 let totalEfectivoCaja=0;
 let descuentoEfectivo=0;
+let retiro=0;
     while(menu!=5){ //pide los precios de los prodcutos que vamos a llevar
         menu = parseInt(prompt("¿Que operacion desea realizar?\n1.Cobrar\n2.Retirar dinero de la caja\n3.Ver balance de caja\n4.Ver los montos totales de todas las operaciones realizadas\n5.Salir"));
        
 
           switch (menu) {
             case 1: // Cobrar
-              do {
-                      precio = parseInt(prompt("Ingrese el precio del producto, para finalizar ingrese 0"));
+              do {    //ingreso los precios del producto, hasta que el cajero ingrese 0
+                      precio = parseInt(prompt("Ingrese el precio del producto, PARA FINALIZAR INGRESE 0 "));
                       suma = suma + precio;
                       alert(`El total es igual a ${suma} pesos`);
               } while (precio != 0);
-
-                  let abono = parseInt(prompt("¿Con qué desea abonar?\n1.efectivo\n2.tarjeta"));
-                  if (abono===1) {
-                      efectivo=suma;
-                      totalEfectivoCaja = efectivo+totalEfectivoCaja;
-                      totalVentasEfectivo=efectivo+totalVentasEfectivo;
-                  }else if(abono===2){
-                    tarjeta=suma;
-                    
-                  }
-                  totalVentasTarjeta=totalVentasTarjeta + tarjeta;
+                      let abono = parseInt(prompt("¿Con qué desea abonar?\n1.efectivo\n2.tarjeta"));
+                      if (abono===1) {
+                          efectivo=suma;   //asigno suma a efectivo para despues descriminar las ventas
+                      }else if(abono===2){
+                        tarjeta=suma;     // asigno suma a tarjeta para despues descriminar las ventas
+                      }
                  
-                  
                   if (abono === 1 && efectivo > 2000 && efectivo<=5000) {
                       descuento = efectivo * 0.05;
                       efectivo = efectivo - descuento;
+                      totalVentasEfectivo=efectivo+totalVentasEfectivo;// voy sumando las ventas en efectivo
+                      totalEfectivoCaja = efectivo+totalEfectivoCaja;// voy sumando para mi caja
                       alert(`El monto a pagar con descuento es de ${efectivo} pesos`);}
-                    else if (abono === 1 && efectivo <=2000){
-                      alert(`El monto a pagar en efectivo sin descuento es de ${efectivo}pesos`);
+                  else if (abono === 1 && efectivo <=2000){
+                      totalVentasEfectivo=efectivo+totalVentasEfectivo;// voy sumando las ventas en efectivo
+                      totalEfectivoCaja = efectivo+totalEfectivoCaja;// voy sumando mi caja
+                      alert(`El monto a pagar en efectivo sin descuento es de ${efectivo} pesos`);
                     }
-                  if (abono === 1 && efectivo > 5000) {
+                  else if (abono === 1 && efectivo > 5000) {
                       descuentoEfectivo = efectivo * 0.10;
                       efectivo = efectivo - descuentoEfectivo;
-                      alert(`El monto con descuento incluido es ${efectivo}pesos`);
-              }
-                  else if ( abono === 2 && tarjeta > 5000){
+                      totalVentasEfectivo=efectivo+totalVentasEfectivo; //voy sumando las ventas en efectivo
+                      totalEfectivoCaja = efectivo+totalEfectivoCaja;//voy sumando mi caja
+                      alert(`El monto con descuento incluido es ${efectivo} pesos`);
+                     }
+                  if ( abono === 2 && tarjeta > 5000){
                       descuento= tarjeta *0.10;
                       tarjeta= tarjeta-descuento;
+                      totalVentasTarjeta=totalVentasTarjeta + tarjeta;// voy sumando las ventas en tarjeta, segun condicion
                       alert(`El monto a pagar con tarjeta con descuento es de ${tarjeta}pesos`);
-                    }else if ( abono === 2 && tarjeta < 5000) {
-                      
+                     }
+                  else if ( abono === 2 && tarjeta < 5000) {
+                      totalVentasTarjeta=totalVentasTarjeta + tarjeta;// voy sumando las ventas en tarjeta 
                       alert(`El monto a pagar con tarjeta sin descuento es ${tarjeta}pesos`);
                     }
-                  
+                  // nos pregunta con cuanto pagamos y devolvemos si corresponde
                   if (abono === 1) {
                     pago = parseInt(prompt('¿Con cuánto quiere pagar?'));               
-                    if (pago >= efectivo) {
+                  if (pago >= efectivo) {
                       vuelto = pago - efectivo;
                       alert('Su vuelto es: ' + vuelto);              
-                }else{
+                  } 
+                  else{
                       alert('Su dinero No es sufiente');
                 }
             
               }
                 break;    
             case 2://retirar dinero de la caja
-                retiro = parseInt(prompt('¿Cuánto dinero desea retirar?'));
-              
-               
-                if (totalEfectivoCaja > retiro) {
-                        retirocaja = totalEfectivoCaja - retiro ;
-                        alert(`Usted retiro ${retiro} pesos y hora su saldo de caja es ${retirocaja} pesos`);
-                }else {
-                        alert(`Fondos insuficientes, usted quiso retirar ${retiro} y su fondo de caja es de ${totalEfectivoCaja} pesos`);
-                }
-              break; 
+                  retiro = parseInt(prompt('¿Cuánto dinero desea retirar?'));
+                   if (totalEfectivoCaja > retiro) {
+                      retirocaja = totalEfectivoCaja - retiro ;
+                      alert(`Usted retiro ${retiro} pesos y hora su saldo de caja es ${retirocaja} pesos`);
+                      }
+                   else {
+                      alert(`Fondos insuficientes, usted quiso retirar ${retiro} y su fondo de caja es de ${totalEfectivoCaja} pesos`);
+                      }
+               break; 
               
             case 3:// balance de caja
-                balance = (parseInt(prompt("Ver con que se pago:\n1.Efectivo\n2.Tarjeta")));
-                if (balance===1) {
-                  alert(`Se pago con efectivo una suma de ${totalVentasEfectivo-descuentoEfectivo} pesos`);
-                }else if(balance===2 && tarjeta!=0) {
-                
-                  alert(`se pago con tarjeta un monto de ${totalVentasTarjeta} pesos`)
-                } else{
-                  alert("Por el momento no hay compras con tarjetas")
-                }
+                  balance = (parseInt(prompt("Ver total con que se pago:\n1.Efectivo\n2.Tarjeta")));
+                  if (balance===1) {
+                      alert(`Se pago con efectivo una suma de ${totalVentasEfectivo} pesos`);
+                  }
+                  else if(balance===2 && tarjeta!=0) {
+                      alert(`se pago con tarjeta un monto de ${totalVentasTarjeta} pesos`)
+                  } 
+                  else{
+                      alert("Por el momento no hay compras con tarjetas")
+                  }
               break;
-            case 4:// monto total de las operaciones
-            
+            case 4:// monto total de las operaciones 
                   alert(`El monto total de las operaciones tanto de compras en efectivo y compras con tarjeta  es igual a ${totalVentasEfectivo+totalVentasTarjeta} pesos`);
-              
-                break;
+               break;
             
               default:
                 alert("Gracias por usar Nuestro sistema");
                 break;
           }
-            suma=0;
+            suma=0;// esto lo que hace que cada vez que entre a la seccion cobrar me reinicie la suma.
           
 }
